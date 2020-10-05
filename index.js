d3e6916481a10015befc15dd3e588f9207708743
Amanda Westin <69770137+a-westin@users.noreply.github.com>
2020-10-05 19:57:17 -0400

    required 'inquirer', 'fs', and 'generateMarkdown' function. defined array of user questions

---
+++ index.js
@@ -0,0 +1,67 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const generateMarkdown = require("./utils/generateMarkdown");
+
+// array of questions for user
+const questions = [
+  {
+    type: "input",
+    message: "What is your project title?",
+    name: "title",
+  },
+  {
+    type: "input",
+    message: "What is the name of your repository?",
+    name: "repository",
+  },
+  {
+    type: "input",
+    message: "Please provide a description of your project.",
+    name: "description",
+  },
+  {
+    type: "input",
+    message: "What are the installation instructions?",
+    name: "installation",
+  },
+  {
+    type: "input",
+    message: "Please provide any necessary usage information.",
+    name: "usage",
+  },
+  {
+    type: "input",
+    message: "Please provide any guidelines for contributing to your project.",
+    name: "guidelines",
+  },
+  {
+    type: "input",
+    message: "What are the instructions for testing your project?",
+    name: "testing",
+  },
+  {
+    type: "checkbox",
+    message: "Please choose a license.",
+    name: "license",
+    choices: ["MIT", "Mozilla", "ODbl", "IBM"],
+  },
+  {
+    type: "input",
+    message: "What is your GitHub username?",
+    name: "ghUsername",
+  },
+  {
+    type: "input",
+    message: "What is your email address?",
+    name: "email",
+  },
+];
+
+// function to write README file
+function writeToFile(fileName, data) {}
+
+// function to initialize program
+function init() {}
+
+// function call to initialize program
+init();
